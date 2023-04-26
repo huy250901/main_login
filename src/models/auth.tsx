2675661d1311b6a52195109bfcf629347080b4a9
Duc Huy <huyyy250901@gmail.com>
2023-04-27 01:31:10 +0700

    commit99

--- src/models/auth.tsx
+++ src/models/auth.tsx
@@ -30,16 +30,9 @@ export interface ICity {
   name: string;
 }
 
-<<<<<<< HEAD
-interface ImageCropProps {
-  src: string;
-  aspectRatio: number;
-  setCropData: (cropData: string) => void;
-=======
 export interface GetUser {
   id: number;
   name: string;
   email: string;
   error?: null;
->>>>>>> e4f475addb9a527375ec095478a92594e96bb175
 }
